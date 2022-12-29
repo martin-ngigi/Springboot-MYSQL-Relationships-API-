bcd3c0b5f5e195fb1db1bb6f829d01c63421bf84
martin-ngigi <martinwainaina001@gmail.com>
2022-12-29 12:52:13 +0300

    Initial commit

---
+++ src/main/java/com/martin/sqlrelationships/SqlRelationshipsApplication.java
@@ -0,0 +1,13 @@
+package com.martin.sqlrelationships;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlRelationshipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlRelationshipsApplication.class, args);
+	}
+
+}
